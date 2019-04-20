48133b224f41ee2d915b1c37821f3d91a51f3697
Mohammad Rehaan <rehaan@Mohammads-iMac.local>
2019-04-19 21:48:24 -0400

    add the algorithm bro...but not working yet ....bro!

--- api_v1/utils/mapUtils.py
+++ api_v1/utils/mapUtils.py
@@ -1,41 +1,83 @@
 import random
 import copy
 
+def getState(cell):
+    if cell['has_flag'] == True:
+        return 3
+    elif cell['has_flag'] == False and cell['is_revealed'] == False:
+        return 0
+    elif cell['is_revealed'] == True and cell['has_mine'] == True:
+        return 2
+    elif cell['is_revealed'] == True:
+        return 1
+
+
+def markCellVisited(map_state, r, c):
+    map_state[r][c]['adj'] = getMinesAround(map_state, r, c)
+    map_state[r][c]['is_revealed'] = True
+    map_state[r][c]['state'] = 1
+    return map_state
+
+
 def makeMove(move, map_state, map_original):
 
     intent = move['intent']
-    cell = move['cell'].split("-")
+    cell_index = move['cell'].split("-")
 
-    row = int(cell[0])
-    col = int(cell[1])
-
-    # print(row, col, map_original)
+    row = int(cell_index[0])
+    col = int(cell_index[1])
 
     def reveal(map_state, r, c):
+
         if (r < 0 or c < 0 or r >= len(map_state) or c >= len(map_state[0])):
-            return map_state
+            return
 
-        if map_state[r][c]['state'] == 2:
-            return map_state
+        current_cell_state = getState(map_state[r][c]) 
+
+        if current_cell_state == 1:
+            return
+
+        if current_cell_state == 0:
+            map_state = markCellVisited(map_state, r, c)
+
+        reveal(map_state, r+1, c)
+        reveal(map_state, r-1, c)
+        reveal(map_state, r, c+1)
+        reveal(map_state, r, c-1)
+        reveal(map_state, r+1, c+1)
+        reveal(map_state, r+1, c-1)
+        reveal(map_state, r-1, c+1)
+        reveal(map_state, r-1, c-1)
 
 
-    cell_state = map_original[row][col]['state']
-    print(cell_state)
+    cell = map_original[row][col]
+    
+    cell_state = getState(cell)
 
+    # Intent is to reveal the cell
     if intent == 'reveal':
+
+        mines_around = getMinesAround(map_original, row, col)
+
+        if mines_around > 0:
+            return markCellVisited(map_original, row, col)
+
         if cell_state == 0:
-            return map_state
+            reveal(map_state, row, col)
         elif cell_state == 1:
-            reveal(map_original, row, col)
-        elif cell_state == 2:
-            pass
-        elif cell_state == 3:
-            pass
-    elif intent == 'flag':
-        if cell_state == 0 or cell_state == 2:
-            map_state[row][col]['state'] = 3
-        elif cell_state == 1 or cell_state == 3:
             return map_state
+        elif cell_state == 2:
+            return map_state
+        elif cell_state == 3:
+            return map_state
+
+    # intent is to flag this cell
+    elif intent == 'flag':
+
+        if cell_state == 0:
+            cell.has_flag = True
+        else:
+            cell.has_flag = False
 
     return map_state
 
@@ -50,7 +92,13 @@ def getNewMap():
     for i in range(row):
         new_map.append([])
         for j in range(col):
-            new_map[i].append({"adj": 0, "state": 0})
+            new_map[i].append({
+                "adj": 0, 
+                "state": 0,
+                "has_mine": False,
+                "is_revealed": False,
+                "has_flag": False
+            })
     
     map_state = copy.deepcopy(new_map)
 
@@ -59,11 +107,33 @@ def getNewMap():
     for i in range(rows_for_mines):
         rand_row = random.randint(0, row-1)
         cells_for_mines = random.randint(0, col)
-        print(cells_for_mines)
         for j in range(cells_for_mines):
             rand_cell = random.randint(0, col-1)
-            print(new_map[rand_row][rand_cell])
-            new_map[rand_row][rand_cell]['state'] = 2
+            new_map[rand_row][rand_cell]['has_mine'] = True
 
     map_original = copy.deepcopy(new_map)
     return map_state, map_original
+
+
+def getMinesAround(map_state, row, col):
+    mines = 0
+    max_rows = len(map_state) - 1
+    max_cols = len(map_state[0]) - 1
+
+    def findMine(row, col, max_rows, max_cols, map_state):
+        if row > 0 and row <= max_rows and col > 0 and col <= max_cols:
+            cell = map_state[row][col]
+            if cell['has_mine'] == True:
+                return 1
+        return 0
+
+    mines = mines + findMine(row+1, col, max_rows, max_cols, map_state)        
+    mines = mines + findMine(row-1, col, max_rows, max_cols, map_state)        
+    mines = mines + findMine(row, col+1, max_rows, max_cols, map_state)        
+    mines = mines + findMine(row, col-1, max_rows, max_cols, map_state)        
+    mines = mines + findMine(row+1, col+1, max_rows, max_cols, map_state)        
+    mines = mines + findMine(row+1, col-1, max_rows, max_cols, map_state)        
+    mines = mines + findMine(row-1, col+1, max_rows, max_cols, map_state)        
+    mines = mines + findMine(row-1, col-1, max_rows, max_cols, map_state)        
+    
+    return mines
--- api_v1/views.py
+++ api_v1/views.py
@@ -12,7 +12,23 @@ class GameViewSet(viewsets.ModelViewSet):
 
     queryset = Game.objects.all()
     serializer_class = GameSerializer
+
+    def perform_create(self, serializer):
+        map_state, map_original = mapUtils.getNewMap()
+        serializer.save(
+            name=self.request.data.get('name'),
+            uuid=self.request.data.get('uuid'),
+            map_state=json.dumps(map_state),
+            map_original=json.dumps(map_original)
+        )
     
+
+    def retrieve(self, request, pk=None):
+        queryset = self.get_queryset()
+        user = get_object_or_404(queryset, pk=self.kwargs['pk'])
+        return Response({'name': user.name, 'uuid': user.uuid, 'map_state': json.loads(user.map_state)})
+
+
     @action(methods=['put'], detail=False)
     def move(self, request):
         queryset = self.get_queryset()
@@ -29,39 +45,19 @@ class GameViewSet(viewsets.ModelViewSet):
         else:
             return Response({'error': 'some error'})
     
+
     @action(methods=['put'], detail=False)
     def new(self, request):
         queryset = self.get_queryset()
         user = get_object_or_404(queryset, pk=request.data.get('uuid'))
         serializer = GameSerializer(user, data=request.data)
-        new_map_state = json.dumps(mapUtils.getNewMap())
+        new_map_state, new_map_original = mapUtils.getNewMap()
         if serializer.is_valid():
             serializer.save( 
-                map_state=new_map_state
+                map_state=json.dumps(new_map_state),
+                map_original=json.dumps(new_map_original)
             )
-            return Response({'user': request.data, 'new_map_state': json.loads(new_map_state)})
-        else:
-            return Response({'error': 'some error'})
-    
-    def retrieve(self, request, pk=None):
-        queryset = self.get_queryset()
-        user = get_object_or_404(queryset, pk=self.kwargs['pk'])
-        serializer = GameSerializer(user, data=request.data)
-        if serializer.is_valid():
-            move = request.data.get('move')
-            new_map_state = mapUtils.makeMove(move, json.loads(request.data.get('map_state')))
-            serializer.save( 
-                map_state=new_map_state
-            )
-            return Response({'new_map_state': new_map_state})
+            return Response({'user': request.data, 'new_map_state': new_map_state})
         else:
             return Response({'error': 'some error'})
 
-    def perform_create(self, serializer):
-        map_state, map_original = mapUtils.getNewMap()
-        serializer.save(
-            name=self.request.data.get('name'),
-            uuid=self.request.data.get('uuid'),
-            map_state=json.dumps(map_state),
-            map_original=json.dumps(map_original)
-        )
